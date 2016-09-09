19bce88496e093d2c04e3d3249f478f9307e78ad
cpotdevin <c.potdevin10@uniandes.edu.co>
2016-09-09 18:43:56 -0500

    Finished Cliente

--- caso1/src/cliente/Cliente.java
+++ caso1/src/cliente/Cliente.java
@@ -1,7 +1,23 @@
 package cliente;
 
-public class Cliente {
-	public Cliente() {
-		
+import caso1.Buffer;
+import caso1.Mensaje;
+
+public class Cliente extends Thread{
+	
+	private int numMensajes;
+	
+	private Buffer buffer;
+	
+	public Cliente(int numMensajes, Buffer buffer) {
+		this.numMensajes = numMensajes;
+		this.buffer = buffer;
+	}
+	
+	public void run() {
+		while (0 < numMensajes) {
+			Mensaje m = new Mensaje((int)Math.random()*101);
+			buffer.enviar(m);
+		}
 	}
 }
