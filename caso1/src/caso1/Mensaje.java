d0af83d25236c8abbda30941519d02a96a6c9df0
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2016-09-09 17:05:53 -0500

    Added attribute valor to Mensaje

--- caso1/src/caso1/Mensaje.java
+++ caso1/src/caso1/Mensaje.java
@@ -1,5 +1,20 @@
 package caso1;
 
 public class Mensaje {
+	
+	private int valor;
+	
+	public Mensaje (int valor) {
+		this.valor = valor;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
 
 }
