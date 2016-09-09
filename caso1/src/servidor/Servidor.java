9dcc9cdc7550e0d78efe77bc941230689bc13e8d
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2016-09-09 18:30:03 -0500

    Read data from setup.properties in Servidor

--- caso1/src/servidor/Servidor.java
+++ caso1/src/servidor/Servidor.java
@@ -1,17 +1,21 @@
 package servidor;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import caso1.Buffer;
 
-public class Servidor {
+public class Servidor extends Thread {
+	
+	private int id;
 	
 	private Buffer buffer;
 	
-	private int nThreads;
-
-	public Servidor(Buffer buffer, int nThreads) {
+	public Servidor(int id, Buffer buffer) {
 		super();
 		this.buffer = buffer;
-		this.nThreads = nThreads;
 	}
 
 	public Buffer getBuffer() {
@@ -21,13 +25,42 @@ public class Servidor {
 	public void setBuffer(Buffer buffer) {
 		this.buffer = buffer;
 	}
-
-	public int getnThreads() {
-		return nThreads;
-	}
-
-	public void setnThreads(int nThreads) {
-		this.nThreads = nThreads;
+	
+	public void run() {
+		
 	}
 	
+	public static void main (String[] args) {
+		Properties prop = new Properties();
+		try {
+			//Cargar el archivo properties
+			FileInputStream in = new FileInputStream("docs/setup.properties");
+			prop.load(in);
+			in.close();
+			
+			//Leer las propiedades
+			int nThreads = Integer.parseInt(prop.getProperty("numServidores"));
+			String[] threadsClientes = (prop.getProperty("numsConsultasPorCliente")).split(",");
+			int[] nThreadsPorCliente = new int[threadsClientes.length];
+			int nClientes = threadsClientes.length;
+			for (int i=0; i<threadsClientes.length; i++){
+				nThreadsPorCliente[i] = Integer.parseInt(threadsClientes[i]);
+			}
+			int tamBuffer = Integer.parseInt("tamBuffer");
+			
+			//Crear los componentes
+			//Buffer b = new Buffer(tamBuffer, nClientes);
+			Servidor[] threads = new Servidor[nThreads];
+			for (int i=0; i<nThreads; i++) {
+				threads[i] = new Servidor(i, null);
+				threads[i].start();
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
