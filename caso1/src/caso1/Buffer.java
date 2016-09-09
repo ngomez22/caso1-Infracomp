41a17b231f8067ae47b1ffe52066fbc90bb412db
cpotdevin <c.potdevin10@uniandes.edu.co>
2016-09-09 18:00:43 -0500

    Updated Buffer methods

--- caso1/src/caso1/Buffer.java
+++ caso1/src/caso1/Buffer.java
@@ -1,5 +1,22 @@
 package caso1;
 
+import java.util.ArrayList;
+
 public class Buffer {
 	
+	private int tam;
+	
+	private ArrayList<Mensaje> mensajes;
+	
+	public Buffer(int tam) {
+		this.tam = tam;
+	}
+	
+	public synchronized void escribir(Mensaje m) {
+		while (tam <= mensajes.size()) {
+			Thread.yield();
+		}
+		mensajes.add(m);
+		m.esperar();
+	}
 }
